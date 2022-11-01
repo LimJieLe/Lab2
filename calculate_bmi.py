3c7d713ed43b783729add78e10db607d33aa7460
LimJieLe <JIELE.21@ichat.sp.edu.sg>
2022-11-01 10:31:48 +0800

    Lab 2

--- calculate_bmi.py
+++ calculate_bmi.py
@@ -5,10 +5,11 @@ def calculate_bmi(height, weight):
     print("BMI = " + str(bmi))
     if bmi < 18.5 :
         print("Under Weight")
+        return(-1)
     elif bmi >= 18.5 and bmi <= 25.0:
         print("Normal Weight")
+        return(0)
     elif bmi > 25.0 :
         print("Over Weight")
-
-calculate_bmi(weight=57, height=1.73)
+        return(1)
 
